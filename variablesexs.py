f8c45b3f7f5feda156fc47928fa40823a8fce422
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-22 17:11:05 +0100

    Add files via upload

---
+++ variablesexs.py
@@ -0,0 +1,11 @@
+machaine = "bonjour"
+monflottant = 10.0
+myint = 20
+
+if machaine == "bonjour" :
+    print("Chaine : %s" % machaine)
+if isinstance(monflottant, float) and monflottant == 10.0 : 
+    print("Flottant : %f" % monflottant)
+if isinstance(myint,int) and myint == 20 : 
+    print("Entier : %d" % myint)
+    
