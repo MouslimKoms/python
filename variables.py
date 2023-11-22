a032441050589697b0d4236319d347c9e1dfdfdb
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-22 17:22:50 +0100

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,60 @@
+
+
+
+print("pour définir un entier")
+
+print("entier")
+int = 7
+print(int)
+
+
+print("pour définir un flottante")
+
+print("monflottant1")
+myfloat = 7.0 
+print(myfloat)
+
+print("autre téchnique pour définr un nombre flottante")
+
+print("monflottant2")
+monflottant = float(7)
+print(monflottant)
+
+
+print("pour définir une chaine")
+
+print("chaine")
+machaine = 'voici ma chaine' 
+print(machaine)
+
+
+print("autre téchnique pour définr une chaine ")
+
+print("chaine avec apostrophe")
+machaine2 = "Ne t'inquite pas pour les apostrophes avec cette téchnique de double guillemets"
+print(machaine2)
+
+
+print("Opérateur simple.Il penvent etre exécutés sur des nombres et des chaines de caractères")
+
+print("Opérateur simple")
+un = 1 
+deux = 2 
+trois = un + deux 
+print(trois)
+
+print("autre téchnique Opérateur simple avec double guillemets")
+
+personne = "Kom's"
+salut = "Bonjour"
+
+salutation = "Bonjour" + "kom's"
+print(salutation)
+
+
+print("afféctation")
+
+a,b = 7,9
+print(a,b)
+
+
