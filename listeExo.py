6cd4143dad317ef1d2dc88e991a0f9331f0f511f
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-24 03:02:04 +0100

    Add files via upload

---
+++ listeExo.py
@@ -0,0 +1,18 @@
+nombres = []
+chaînes = []
+noms = ["John", "Eric", "Jessica"]
+
+# écrivez votre code ici
+nombres.append(1)
+nombres.append(2)
+nombres.append(3)
+
+chaînes.append("bonjour")
+chaînes.append("monde")
+
+second_name = noms[1]
+
+# ce code doit écrire les tableaux remplis et le deuxième nom dans la liste des noms (Eric).
+print(nombres)
+print(chaînes)
+print("Le deuxième nom sur la liste des noms est %s" % second_name)
