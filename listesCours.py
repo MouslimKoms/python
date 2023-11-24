5817972802fba243e9600f56beb1e82592a62df3
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-24 03:03:08 +0100

    Add files via upload

---
+++ listesCours.py
@@ -0,0 +1,11 @@
+inventaire = []
+inventaire.append(1)
+inventaire.append(2)
+inventaire.append(3)
+print(inventaire[0]) # prints 1
+print(inventaire[1]) # prints 2
+print(inventaire[2]) # prints 3
+
+# prints out 1,2,3
+for x in inventaire:
+    print(x)
