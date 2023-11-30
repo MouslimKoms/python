57d78dc6a44de0c3e1015f3623cd256f4af94dd2
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-11-30 15:03:05 +0100

    Add files via upload

---
+++ formatage_de_chaine.py
@@ -0,0 +1,16 @@
+name = "John"
+print("Hello, %s!" % name)
+
+
+name = "John"
+age = 23
+print("%s à %d ans." % (name, age))
+
+
+ma_liste = [1,2,3]
+print("liste A: %s" % ma_liste)
+
+data = ("Mouslim", "Koms", 53.44)
+format_string = "Bonjour %s %s. votre solde actuel est de %s$."
+
+print(format_string % data)
