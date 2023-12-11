26988f3487fd27295b247c56a37146726a7a19a1
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-12-11 19:43:01 +0100

    Add files via upload

---
+++ cours_boucles.py
@@ -0,0 +1,12 @@
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# Prints out 6,7,8
+for x in range(6, 9):
+    print(x)
+   
+   # Prints out 9,11,13,
+for x in range(9, 15, 2):
+    print(x)
+    
