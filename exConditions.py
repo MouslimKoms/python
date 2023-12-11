7f9eed5c9d5598ee6b93bf8abc3bc050bc836e70
MouslimKoms <140436051+MouslimKoms@users.noreply.github.com>
2023-12-11 16:02:13 +0100

    Add files via upload

---
+++ exConditions.py
@@ -0,0 +1,23 @@
+
+number = 16
+second_number = 0
+first_tableau = [1,2,3]
+second_tableau = [1,2]
+
+if number > 15:
+    print("1")
+
+if first_tableau:
+    print("2")
+
+if len(second_tableau) == 2:
+    print("3")
+
+if len(first_tableau) + len(second_tableau) == 5:
+    print("4")
+
+if first_tableau and first_tableau[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
